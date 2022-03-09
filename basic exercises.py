67a834fda4d346cf0d6a68279d53a19fd15610f2
ציונה אלמיו <tsionalemayo73@gmail.com>
2022-03-09 19:41:03 +0200

    basic exercises

---
+++ basic exercises.py
@@ -0,0 +1,47 @@
+#1
+# x=int(input("enter number"))
+# print(pow(x,2))
+#
+# #2
+# age=int(input("enter your age"))
+# print("your age in 2o year will be:", age+20)
+
+#3
+# price=int(input("enter price"))
+# print(price*1.17)
+
+#4
+# num1=int(input("enter number"))
+# num2=int(input("enter number"))
+#
+# print(num1+num2)
+# print(num1-num2)
+# print(num1*num2)
+# print(num1/2)
+
+#5
+# print("welcome\ndate: 9/3/22 wednesday")
+
+#6
+# num1=int(input("enter number"))
+# num2=int(input("enter number"))
+# num3=int(input("enter number"))
+# num4=int(input("enter number"))
+#
+# print((num1+num2+num3+num4)/4)
+
+#7
+# x=int(input("enter nails numbers:"))
+# print("the number of the boxs:",x/40)
+
+#8
+#זמן*מהירות=קמש
+# x=int(input("enter the distance"))
+# y=int(input("enter the the speed"))
+#
+# print("the Arrival time in hours:",x/y)
+# print("the arrival tine in minutes",(x/y)*60)
+
+#10
+# tel=input("enter your phone number without the first number")
+# print("0"+tel)
